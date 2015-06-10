dfafe7eb962e169f35b3c86c16e465baef2a1588
lmpizarro <lmpbici@gmail.com>
2015-06-09 22:38:23 -0300

    testing client rest wordpress

--- hello.py
+++ hello.py
@@ -3,3 +3,25 @@ print("hola mundo")
 print("made a change in master")
 print("made a hot fix")
 print("branch testing")
+
+'''
+http://www.jaimegil.me/2012/12/26/a-python-restful-api-consumer.html
+https://www.npmjs.com/package/wordpress-rest-api
+search: javascript api for rest wordpress
+https://wordpress.org/plugins/json-rest-api/
+https://developer.wordpress.com/docs/rest-api-javascript/
+'''
+
+import requests
+
+response = requests.get('http://testwp-c9-lmpizarro.c9.io/wp-json/media')
+
+assert response.status_code == 200
+
+for repo in response.json():
+    #print '[{}] {}'.format(repo['language'], repo['name'])
+    print repo["source"]
+    #for k in repo:
+    #    print ("%s -------> %s "%(k, repo[k]))
+
+
